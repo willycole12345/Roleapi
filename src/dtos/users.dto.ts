25ab6e576d1d5b3e5121cbf5d27c8d7ae6d8f3fa
william cole <cole.william043@gmail.com>
2025-01-13 18:28:12 +0100

    first commit

---
+++ src/dtos/users.dto.ts
@@ -0,0 +1,29 @@
+import { IsEmail, IsString, IsNotEmpty, MinLength, MaxLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  public email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(9)
+  @MaxLength(32)
+  public password: string;
+
+  @IsString()
+  public firstname: string;
+
+  @IsString()
+  public lastname: string;
+
+  @IsString()
+  public group: string;
+}
+
+export class UpdateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(9)
+  @MaxLength(32)
+  public password: string;
+}
---
+++ src/models/users.model.ts
@@ -0,0 +1,16 @@
+import { Model, ModelObject } from 'objection';
+import { User } from '@interfaces/users.interface';
+
+export class UserModel extends Model implements User {
+  id!: number;
+  email!: string;
+  firstname!: string;
+  lastname!: string;
+  group!: string;
+  password!: string;
+
+  static tableName = 'users'; // database table name
+  static idColumn = 'id'; // id column name
+}
+
+export type UserShape = ModelObject<UserModel>;
